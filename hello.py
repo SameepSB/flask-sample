9cda9589726f1cb8518e7c317b30a42dd969941b
sameep <sameeplcm@gmail.com>
2022-08-07 16:44:20 +0530

    update with Dockerfile

--- hello.py
+++ hello.py
@@ -5,10 +5,10 @@ from flask import request
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return "<h1>Hello world from Kubernetes!</h1>"
+    return '<h1>Hello world from Kubernetes!</h1>'
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
